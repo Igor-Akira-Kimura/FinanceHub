5c6530832da8653740096ee2d48e86e9313e8945
Igor-Akira-Kimura <igorkimura260@gmail.com>
2026-08-25 10:59:05 -0300

    feat: implementando trace no login

---
+++ FinanceHub.Application/Observability/FinanceHubActivitySource.cs
@@ -0,0 +1,9 @@
+﻿using System.Diagnostics;
+
+namespace FinanceHub.Application.Observability;
+
+public static class FinanceHubActivitySource
+{
+    public static readonly ActivitySource Instance =
+        new("FinanceHub");
+}
